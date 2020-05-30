6eb943752a272cc477410c18314af1b7cb5fb8e3
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2020-05-30 13:38:47 -0700

    Create remove-duplicates.py

---
+++ interviewee/arrays/remove-duplicates.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        p = 1
+        for i in range(1,len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[p] = nums[i]
+                p += 1
+        print(nums[:p])
+© 2020 GitHub, Inc.
