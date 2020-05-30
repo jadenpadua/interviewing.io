e3081eb27e71f41f8dd989b7a094f8f95a57ef5f
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2020-05-30 13:37:09 -0700

    Create permutations.py

---
+++ interviewer/backtracking/permutations.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        self.backtrack(nums,[],res)
+        return res
+
+    def backtrack(self,arr,currPerm,res):
+        if not len(arr):
+            res.append(currPerm)
+        else:
+            for i in range(len(arr)):
+                newArr = arr[:i] + arr[i+1:]
+                newPerm = currPerm + [arr[i]]
+                self.backtrack(newArr,newPerm,res)
+                
